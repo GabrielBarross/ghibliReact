2dc868e370f65476adcd4b68d32385e5f47cb83c
gabrielbarross <gabriel.barros@vtex.com.br>
2019-12-27 16:53:37 -0300

    Alterado Página Principal e adicionado base para página de filmes

--- src/components/Film.js
+++ src/components/Film.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Film = () => {
     return (
         <>
+            <section className="entireSection">
+                <h1 className="entireName">
+                    Movie Name Here
+                </h1>
+                <p className="entireDescription">
 
+                </p>
+            </section>
         </>
     );
 }
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import Header from './Header';
+import GhibliLogo from '../images/ghibliLogo.jpg'
 import Cards from './Cards'
+import Film from "./Film"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 const Main = () => {
 
@@ -18,14 +20,35 @@ const Main = () => {
 
     return (
         <>
-            <Header />
-            <section className="mainSection">
-                {
-                    movieData.map((data, index) => {
-                        return <Cards movieData={data} key={index} />
-                    })
-                }
-            </ section>
+            <Router>
+                <header className="mainHeader">
+                    <img src={GhibliLogo} className="logoImage" />
+                    <h1 className="mainTitle">Ghibli Movies List</h1>
+                    <ul>
+                        <li>
+                            <Link to="/">List</Link>
+                        </li>
+                        <li>
+                            <Link to="/films">Film</Link>
+                        </li>
+                    </ul>
+                </header>
+
+                <Switch>
+                    <Route path="/films">
+                        <Film />
+                    </Route>
+                    <Route path="/">
+                        <section className="mainSection">
+                            {
+                                movieData.map((data, index) => {
+                                    return <Cards movieData={data} key={index} />
+                                })
+                            }
+                        </ section>
+                    </Route>
+                </Switch>
+            </Router>
         </>
     );
 }
